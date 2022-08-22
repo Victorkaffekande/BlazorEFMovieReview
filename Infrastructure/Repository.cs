30513c270a9cafe57c224f537e1efe151c8a6230
Victorkaffekande <90904666+Victorkaffekande@users.noreply.github.com>
2022-08-22 13:48:40 +0200

    setup

--- Infrastructure/Repository.cs
+++ Infrastructure/Repository.cs
@@ -1,5 +1,6 @@
 ﻿using Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 
 namespace Infrastructure;
@@ -7,55 +8,71 @@ namespace Infrastructure;
 public class Repository :  IRepository
 {
 
-    private Movie mockMovieObject;
-    private Review mockReviewObject;
+    
     private DbContextOptions<RepositoryDbContext> _opts;
 
     public Repository()
     {
-        mockMovieObject =new Movie()
-        {
-            Id = 1, Summary = "Bob writes a program ...", Title = "Bob's Movie", ReleaseYear = 2022,
-            BoxOfficeSumInMillions = 42
-        };
-        mockReviewObject = new Review()
-        {
-            Id = 1, Headline = "Super great movie", Rating = 5,
-            ReviewerName = "Smølf", MovieId = 1, Movie = mockMovieObject
-        };
-
         _opts = new DbContextOptionsBuilder<RepositoryDbContext>()
             .UseSqlite("Data source=..//GUI/db.db").Options;
     }
 
     public List<Review> GetReviews()
     {
-        return new List<Review>() { mockReviewObject };
+        using (var Context = new RepositoryDbContext(_opts, ServiceLifetime.Scoped))
+        {
+            return Context.ReviewTable.Include(r => r.Movie).ToList();
+        }
     }
 
     public List<Movie> GetMovies()
     {
-        return new List<Movie>() { mockMovieObject };
+        using (var context = new RepositoryDbContext(_opts,ServiceLifetime.Scoped))
+        {
+            return context.MovieTable.ToList();
+        }
     }
 
     public Movie DeleteMovie(int movieId)
     {
-        return new Movie();
+        using (var context = new RepositoryDbContext(_opts,ServiceLifetime.Scoped))
+        {
+            var movie = context.MovieTable.Find(movieId);
+            context.MovieTable.Remove(movie ?? throw new InvalidOperationException());
+            context.SaveChanges();
+            return movie;
+        }
     }
 
     public Review DeleteReview(int reviewId)
     {
-        return new Review();
+        using (var context = new RepositoryDbContext(_opts, ServiceLifetime.Scoped))
+        {
+            var review = context.ReviewTable.Find(reviewId);
+            context.ReviewTable.Remove(review ?? throw new InvalidOperationException());
+            context.SaveChanges();
+            return review;
+        }
     }
 
     public Movie AddMovie(Movie movie)
     {
-        return movie;
+        using (var context = new RepositoryDbContext(_opts,ServiceLifetime.Scoped))
+        {
+            context.MovieTable.Add(movie);
+            context.SaveChanges();
+            return movie;
+        }
     }
 
     public Review AddReview(Review review)
     {
-        review.Movie = new Movie();
+        using (var context = new RepositoryDbContext(_opts,ServiceLifetime.Scoped))
+        {
+            review.Movie = context.MovieTable.Find(review.MovieId);
+            context.ReviewTable.Add(review);
+            context.SaveChanges();
+        }
         return review;
     }
 }
